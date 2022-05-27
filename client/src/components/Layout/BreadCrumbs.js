49bd3a2ba68d9106767223d889e6add5146ba879
Cedric <c.moyere.dev@gmail.com>
2022-05-27 23:33:11 +0200

    Refacto Breadcrumbs cmp

--- client/src/components/Layout/BreadCrumbs.js
+++ client/src/components/Layout/BreadCrumbs.js
@@ -1,24 +1,27 @@
-import { Link, useLocation, useRouteMatch } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 const BreadCrumbs = () => {
-  const { path, url } = useRouteMatch();
   const pathName = useLocation().pathname;
-  const breadCrumbs = pathName.split("/");
-  breadCrumbs.pop();
-  const key = useLocation().key;
-  return pathName !== "/" ? (
-    <nav aria-label="breadcrumb">
-      <ol className="breadcrumb">
-        {breadCrumbs.map((p) => {
-          return (
-            <li key={p + key} className="breadcrumb-item">
-              <Link to={"/" + p || "/"}>{p || "Home"}</Link>
-            </li>
-          );
-        })}
-      </ol>
-    </nav>
-  ) : (
-    <></>
-  );
+  const Component = () => {
+    const breadCrumbs = pathName.split("/");
+    breadCrumbs.pop();
+    const key = useLocation().key;
+    return (
+      <nav>
+        <ol className="breadcrumb">
+          {breadCrumbs.map((p, i) => {
+            const currPath = breadCrumbs.filter((_, j) => j <= i).join("/");
+            return (
+              <li key={p + key} className="breadcrumb-item">
+                <Link to={currPath}>{p || "Home"}</Link>
+              </li>
+            );
+          })}
+        </ol>
+      </nav>
+    );
+  };
+
+  return pathName !== "/" ? <Component /> : <></>;
 };
 export default BreadCrumbs;
